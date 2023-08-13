3f4b0ed34828ec944b3de3c9bbe197fb0301a297
vytdev <123163428+vytdev@users.noreply.github.com>
2023-08-13 21:25:31 +0800

    deploy: 1a846389de9542f7e0be077c79150778915a5b88

---
+++ assets/js/search.js
@@ -0,0 +1,14 @@
+/* ================================================================
+Search index
+================================================================ */
+doc.addEventListener("ready", function() {
+    
+    // the search function
+    function search(terms) {
+        
+    }
+    
+    doc.addEventListener("indexLoaded", function(index) {
+        
+    });
+});
